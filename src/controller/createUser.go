1f8f2af8b00dcdd616d416342dfe1d3379549f5f
GabrielOGW <gabrielogassawara2001@gmail.com>
2023-03-09 15:03:26 -0300

    feat: create user and validate errors

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,7 +1,24 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"log"
+
+	"github.com/GabrielOGW/go-crud/src/configuration/validation"
+	"github.com/GabrielOGW/go-crud/src/controller/model/request"
+	"github.com/gin-gonic/gin"
+)
 
 func CreateUser(c *gin.Context) {
-	
+	log.Println("init CreateUser controller")
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		log.Printf("Error trying to marshal object, error=%s", err.Error())
+			errRest := validation.ValidateUserError(err)
+
+			c.JSON(errRest.Code, errRest)
+			return
+	}
+	fmt.Println(userRequest)
 }
