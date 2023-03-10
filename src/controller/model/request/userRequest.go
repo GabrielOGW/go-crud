cf2acf4162b59c8b3c993313787b1c3c83c265a1
GabrielOGW <gabrielogassawara2001@gmail.com>
2023-03-10 10:05:13 -0300

    fixing user validation

--- src/controller/model/request/userRequest.go
+++ src/controller/model/request/userRequest.go
@@ -2,7 +2,7 @@ package request
 
 type UserRequest struct {
 	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required,min=6,containsAny=!@#$%&*"`
+	Password string `json:"password" binding:"required,containsany=!@#?,min=6"`
 	Name     string `json:"name" binding:"required,min=4,max=100"`
 	Age      int    `json:"age" binding:"required,numeric,min=1"`
 }
